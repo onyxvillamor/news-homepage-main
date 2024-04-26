b5906e78f37cc859525fa3011120059f702526a1
onyxvillamor <awsgegud420@gmail.com>
2024-04-26 16:46:08 +0800

    almost done

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,5 @@
 const hamburger = document.querySelector(".hamburger");
+const hamburger2 = document.querySelector(".hamburger2");
 const navMenu = document.querySelector(".menu");
 
 hamburger.addEventListener( "click", () => {
@@ -6,7 +7,13 @@ hamburger.addEventListener( "click", () => {
     navMenu.classList.toggle("active");
 })
 
+hamburger2.addEventListener( "click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () =>{
     hamburger.classList.remove("active");
+    hamburger2.classList.remove("active");
     navMenu.classList.remove("active");
 }))
